f49644088a0a746eba1084bc3531681c758a7e21
Francis Duvivier <francisduvivier@gmail.com>
2024-09-18 08:22:12 +0200

    Change run script to connect script since run is not working with relative imports

--- connect.sh
+++ connect.sh
@@ -7,5 +7,5 @@ DEFAULT_DIR="apps/kolab_game"
 APP_DIR="${1:-$DEFAULT_DIR}"
 echo "APP_DIR: [$APP_DIR]"
 
-python3 -m mpremote connect port:$PORT mount $APP_DIR run $APP_DIR/__init__.py
+python3 -m mpremote connect port:$PORT $1
 
